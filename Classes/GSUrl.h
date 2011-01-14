5a8cda4922e2f8196a3306f2834e443ec009b1fd
marciogm <marciogiaxa@gmail.com>
2011-01-14 20:06:20 -0200

    up all base files

---
+++ Classes/GSUrl.h
@@ -0,0 +1,21 @@
+//
+//  GSUrl.h
+//  Google URL Shortener in Objective-c
+//
+//  Created by Marcio Giaxa Marinheiro on 14/01/11.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import "JSON.h"
+#define API_URI @"https://www.googleapis.com/urlshortener/v1/url"
+
+@interface GSUrl : NSObject {
+	NSString *long_url;
+	NSString *short_url;
+	NSString *formatted_url;
+}
+
+-(GSUrl*) initWithString: (NSString*) url;
+-(NSString*) shorten;
+
+@end
---
+++ Classes/GSUrl.m
@@ -0,0 +1,48 @@
+//
+//  GSUrl.m
+//  Google URL Shortener in Objective-c
+//
+//  Created by Marcio Giaxa Marinheiro on 14/01/11.
+//  Copyright 2011 n/a. All rights reserved.
+//
+
+#import "GSUrl.h"
+
+@interface GSUrl ()
+- (NSMutableURLRequest*) prepareRequest;
+- (void) prepareUrl;
+@end
+
+
+@implementation GSUrl
+
+-(GSUrl*) initWithString:(NSString *)url {
+	long_url = url;
+	return self;
+}
+
+-(NSString*) shorten {
+	[self prepareUrl];
+	SBJsonParser *parser = [[SBJsonParser alloc] init];
+	NSMutableURLRequest *preparedRequest = [self prepareRequest];
+	NSData *responseData = [NSURLConnection sendSynchronousRequest:preparedRequest returningResponse:nil error:nil];
+	NSString *json_string = [[NSString alloc] initWithData:responseData encoding:NSASCIIStringEncoding];
+	
+	return [[parser objectWithString:json_string] valueForKey:@"id"];
+}
+
+-(void) prepareUrl {
+	formatted_url = [[NSString alloc] initWithFormat:@"{ \"longUrl\":\"%@\"}", long_url];
+}
+
+-(NSMutableURLRequest*) prepareRequest {	
+	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString: API_URI]];
+	
+	[request setHTTPMethod:@"POST"];
+	[request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+	[request setHTTPBody:[[NSString stringWithString:formatted_url] dataUsingEncoding:NSASCIIStringEncoding]];
+	
+	return request;
+}
+
+@end
